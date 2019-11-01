c826097cffbb4a90de20dae0834a282c4edf75e8
Aaron Hoffer <aaron.hoffer@connexta.com>
2019-11-01 12:49:00 -0700

    Resetable. Changed port to 9500.

--- src/main/java/status/StatusController.java
+++ src/main/java/status/StatusController.java
@@ -4,6 +4,7 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +13,26 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 public class StatusController {
 
-  final Map<String, Job> jobs = new HashMap<>();
-  JobFactory jobFactory = new JobFactory();
+  Map<String, Job> jobs;
+  JobFactory jobFactory;
+
+  public StatusController() {
+    jobFactory = new JobFactory();
+    reset();
+  }
 
   @GetMapping(value = "/job/{jobId}")
   // Synchronize to avoid concurrent modification exception
   public synchronized String status(@PathVariable String jobId) {
     JobStatus status = jobs.computeIfAbsent(jobId, jobFactory::next).getCurrentStatus();
-    if(status.equals(JobStatus.NOTSTARTED)) {
+    if (status.equals(JobStatus.NOTSTARTED)) {
       throw new ResponseStatusException(NOT_FOUND, "Unable to find resource");
     }
     return status.toString();
+  }
+
+  @DeleteMapping("/all")
+  void reset() {
+    jobs = new HashMap<>();
   }
 }
