b48b426a9a97c652c20205d77004e796fd5d9e67
Aaron Hoffer <aaron.hoffer@connexta.com>
2019-11-01 14:08:58 -0700

    Looking pretty good

--- src/main/java/status/StatusController.java
+++ src/main/java/status/StatusController.java
@@ -4,6 +4,7 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import java.util.HashMap;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@Slf4j
 public class StatusController {
 
   Map<String, Job> jobs;
@@ -24,15 +26,21 @@ public class StatusController {
   @GetMapping(value = "/job/{jobId}")
   // Synchronize to avoid concurrent modification exception
   public synchronized String status(@PathVariable String jobId) {
-    JobStatus status = jobs.computeIfAbsent(jobId, jobFactory::next).getCurrentStatus();
+    JobStatus status = lookupStatus(jobId);
+    log.info("Request for {}, responding with {} ", jobId, status.toString());
     if (status.equals(JobStatus.NOTSTARTED)) {
       throw new ResponseStatusException(NOT_FOUND, "Unable to find resource");
     }
     return status.toString();
   }
 
-  @DeleteMapping("/all")
+  private JobStatus lookupStatus(String jobId) {
+    return jobs.computeIfAbsent(jobId, jobFactory::next).getCurrentStatus();
+  }
+
+  @DeleteMapping("/job")
   void reset() {
+    log.info("Resetting state of status endpoint");
     jobs = new HashMap<>();
   }
 }
